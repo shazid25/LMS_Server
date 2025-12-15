4d3e2640a52c41d01e79c53d388c294d6be6b708
shazid25 <ishazid57@gmail.com>
2025-12-15 23:25:11 +0600

    added clerk

--- server.js
+++ server.js
@@ -1,31 +1,3 @@
-// import express from 'express';
-// import cors from 'cors';
-// import 'dotenv/config'
-// import connectDB from './config/mongodb.js';
-// import { clerkWebhooks } from './controllers/webhooks.js';
-
-// //initialize express
-// const app = express();
-
-// //connect to database
-// await connectDB()
-
-// //middlewares
-// app.use(cors())
-
-// //routes
-// app.get('/', (req, res) => {
-//     res.send('Server is running');
-// });
-// app.post('/clerk', express.json(), clerkWebhooks); 
-
-// //port
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
-
-
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
