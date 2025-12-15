722456239259ca73580519b648da6d12d2d463f5
shazid25 <ishazid57@gmail.com>
2025-12-15 20:25:30 +0600

    y

--- server.js
+++ server.js
@@ -1,29 +1,26 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
+import 'dotenv/config'
 import connectDB from './config/mongodb.js';
 import { clerkWebhooks } from './controllers/webhooks.js';
 
+//initialize express
 const app = express();
 
-// middleware
-app.use(cors());
-app.use(express.json());
+//connect to database
+await connectDB()
 
-// connect DB (safe for serverless)
-let isConnected = false;
-async function dbConnect() {
-  if (isConnected) return;
-  await connectDB();
-  isConnected = true;
-}
-dbConnect();
+//middlewares
+app.use(cors())
 
-// routes
+//routes
 app.get('/', (req, res) => {
-  res.send('Server is running');
+    res.send('Server is running');
 });
+app.post('/clerk', express.json(), clerkWebhooks); 
 
-app.post('/clerk', clerkWebhooks);
-
-export default app;
+//port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
