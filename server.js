f3bffa6a50ea1dd03f4ebaac6855ad7923b5b11c
shazid25 <ishazid57@gmail.com>
2025-12-15 20:06:23 +0600

    vercel json added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
 import connectDB from './config/mongodb.js';
+import { clerkWebhooks } from './controllers/webhooks.js';
 
 //initialize express
 const app = express();
@@ -16,6 +17,7 @@ app.use(cors())
 app.get('/', (req, res) => {
     res.send('Server is running');
 });
+app.post('/clerk', express.json(), clerkWebhooks); 
 
 //port
 const PORT = process.env.PORT || 5000;
