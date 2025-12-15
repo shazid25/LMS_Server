a2f071fce81e452107de41db4adf890dbc995474
shazid25 <ishazid57@gmail.com>
2025-12-15 20:44:17 +0600

    fixed the code

--- controllers/webhooks.js
+++ controllers/webhooks.js
@@ -1,11 +1,69 @@
+// import { Webhook } from "svix";
+// import User from "../models/User.js";
+
+
+// //API controller function to manage clerk user with database
+
+
+// const clerkWebhooks = async (req, res) => {
+//     try {
+//         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+//         await whook.verify(JSON.stringify(req.body), {
+//             "svix-id": req.headers["svix-id"],
+//             "svix-timestamp": req.headers["svix-timestamp"],
+//             "svix-signature": req.headers["svix-signature"]
+//         });
+
+//         const { type, data } = req.body;
+
+//         switch (type) {
+//             case "user.created": {
+//                 const userData = {
+//                     _id: data.id,
+//                     email: data.email_addresses[0].email_address,
+//                     name: data.first_name + " " + data.last_name,
+//                     imageUrl: data.profile_image_url
+//                 }
+//                 await User.create(userData);
+//                 res.json({})
+//                 break;
+//             }
+
+//             case 'user.updated': {
+//                 const userData = {
+                  
+//                     email: data.email_addresses[0].email_address,
+//                     name: data.first_name + " " + data.last_name,
+//                     imageUrl: data.profile_image_url
+//                 }
+//                 await User.findByIdAndUpdate(data.id, userData)
+//                 res.json({})
+//                 break;
+//             }
+
+//             case 'user.deleted': {
+//                 await User.findByIdAndDelete(data.id);
+//                 res.json({})
+//                 break;
+//             }
+
+//             default:
+//                 break;
+//         }
+
+//         } catch (error) {
+//         res.json({ message: false, error: error.message })
+//         }
+//     }
+
+
+
 import { Webhook } from "svix";
 import User from "../models/User.js";
 
-
 //API controller function to manage clerk user with database
-
-
-const clerkWebhooks = async (req, res) => {
+export const clerkWebhooks = async (req, res) => {
     try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
@@ -32,7 +90,6 @@ const clerkWebhooks = async (req, res) => {
 
             case 'user.updated': {
                 const userData = {
-                  
                     email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.profile_image_url
@@ -52,7 +109,9 @@ const clerkWebhooks = async (req, res) => {
                 break;
         }
 
-        } catch (error) {
-        res.json({ message: false, error: error.message })
-        }
+    } catch (error) {
+        console.error("Webhook error:", error.message);
+        res.status(400).json({ message: false, error: error.message });
     }
+}
+
--- server.js
+++ server.js
@@ -1,3 +1,31 @@
+// import express from 'express';
+// import cors from 'cors';
+// import 'dotenv/config'
+// import connectDB from './config/mongodb.js';
+// import { clerkWebhooks } from './controllers/webhooks.js';
+
+// //initialize express
+// const app = express();
+
+// //connect to database
+// await connectDB()
+
+// //middlewares
+// app.use(cors())
+
+// //routes
+// app.get('/', (req, res) => {
+//     res.send('Server is running');
+// });
+// app.post('/clerk', express.json(), clerkWebhooks); 
+
+// //port
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
@@ -13,11 +41,16 @@ await connectDB()
 //middlewares
 app.use(cors())
 
+// IMPORTANT: Add express.json() middleware globally to parse JSON
+app.use(express.json());
+
 //routes
 app.get('/', (req, res) => {
     res.send('Server is running');
 });
-app.post('/clerk', express.json(), clerkWebhooks); 
+
+// Webhook route - no need to pass express.json() here since it's applied globally
+app.post('/clerk', clerkWebhooks); 
 
 //port
 const PORT = process.env.PORT || 5000;
