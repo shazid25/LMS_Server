f987202f159d9d1aaeb21d95ac11667677c99719
shazid25 <ishazid57@gmail.com>
2026-01-31 20:07:19 +0600

    added adding new course schema and procedure

--- controllers/educatorController.js
+++ controllers/educatorController.js
@@ -1,4 +1,6 @@
 import { clerkClient } from '@clerk/express'
+import Course from '../models/Course'
+import { v2 as cloudinary } from 'cloudinary'
 
 // update role to educator
 export const updateRoleToEducator = async (req, res) => {
@@ -16,4 +18,31 @@ export const updateRoleToEducator = async (req, res) => {
   } catch (error) {
     res.json({ success: false, message: error.message })
   }
+}
+
+
+//Add new course
+
+export const addCourse = async (req, res)=>{
+    try {
+        const { courseData } = req.body
+        const imageFile = req.file
+        const educatorId = req.auth.userId
+
+        if(!imageFile){
+            return res.json({ success: false, message: 'Thumbnail Not Attached' })
+        }
+
+   const parsedCourseData = await JSON.parse(courseData)
+        parsedCourseData.educator = educatorId
+        const newCourse = await Course.create(parsedCourseData)
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path)
+        newCourse.courseThumbnail = imageUpload.secure_url
+        await newCourse.save()
+
+        res.json({ success: true, message: 'Course Added' })
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
 }
