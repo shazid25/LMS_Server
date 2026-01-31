28319b8069226e4278569a87fa9c0dfa2a243bee
shazid25 <ishazid57@gmail.com>
2026-02-01 00:25:37 +0600

    created api endPoint which will give user data and user enrolled courses

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import educatorRouter from './routes/educatorRoutes.js';
 import { clerkMiddleware } from '@clerk/express';
 import connectCloudinary from './config/cloudinary.js';
 import courseRouter from './routes/courseRoute.js';
+import userRouter from './routes/userRoutes.js';
 
 //initialize express
 const app = express();
@@ -27,6 +28,7 @@ app.get('/', (req, res) => res.send('API is working'));
 app.post('/clerk', express.json(), clerkWebhooks); 
 app.use('/api/educator', express.json(), educatorRouter ); 
 app.use('/api/course', express.json(), courseRouter);
+app.use('/api/user', express.json(), userRouter);
 
 //port
 const PORT = process.env.PORT || 5000;
