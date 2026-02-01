b5b7acc9c4f8985d0d9cf59678820a74016b5b3e
shazid25 <ishazid57@gmail.com>
2026-02-01 11:20:36 +0600

    added payment mathood

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
 import connectDB from './config/mongodb.js';
-import { clerkWebhooks } from './controllers/webhooks.js';
+import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js';
 import educatorRouter from './routes/educatorRoutes.js';
 import { clerkMiddleware } from '@clerk/express';
 import connectCloudinary from './config/cloudinary.js';
@@ -29,6 +29,7 @@ app.post('/clerk', express.json(), clerkWebhooks);
 app.use('/api/educator', express.json(), educatorRouter ); 
 app.use('/api/course', express.json(), courseRouter);
 app.use('/api/user', express.json(), userRouter);
+app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks )
 
 //port
 const PORT = process.env.PORT || 5000;
