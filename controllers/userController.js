65787891b181f853e585717e753f36f0be85699a
shazid25 <ishazid57@gmail.com>
2026-02-02 20:58:22 +0600

    completed the backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -132,3 +132,45 @@ export const getUserCourseProgress = async (req, res) =>{
         res.json({ success: false, message: error.message })
     }
 }
+
+
+// Add User Ratings to Course
+
+export const addUserRating = async (req, res)=>{
+const userId = req.auth.userId;
+const { courseId, rating } = req.body;
+
+if(!courseId || !userId || !rating || rating < 1 || rating > 5){
+    return res.json({ success: false, message: 'InValid Details' });
+}
+
+try {
+    const course = await Course.findById(courseId);
+
+    if(!course){
+        return res.json({ success: false, message: 'Course not found.' });
+    }
+
+    const user = await User.findById(userId);
+
+    if(!user || !user.enrolledCourses.includes(courseId)){
+        return res.json({ success: false, message: 'User has not purchased this course.' });
+    }
+
+    const existingRatingIndex = course.courseRatings.findIndex(r => r.userId === userId)
+
+    if(existingRatingIndex > -1){
+        course.courseRatings[existingRatingIndex].rating = rating;
+    }else{
+        course.courseRatings.push({userId, rating});
+    }
+
+    await course.save();
+
+    return res.json({success: true, message: 'Rating added'})
+
+} catch (error) {
+    return res.json({ success: false, message: error.message });
+}
+
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getUserCourseProgress,
+import { addUserRating, getUserCourseProgress,
          getUserData,
          purchaseCourse,
          updateUserCourseProgress,
@@ -12,6 +12,8 @@ userRouter.get('/enrolled-courses', userEnrolledCourses)
 userRouter.post('/purchase', purchaseCourse)
 userRouter.post('/update-course-progress', updateUserCourseProgress )
 userRouter.post('/get-course-progress', getUserCourseProgress )
+userRouter.post('/add-rating', addUserRating )
+
 
 
 export default userRouter;
