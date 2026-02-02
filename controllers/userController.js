b8191e34dbd6fb0ff8f3d882364b1ad50f62ba2a
shazid25 <ishazid57@gmail.com>
2026-02-02 20:39:11 +0600

    added  get & update UserCourseProgress

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ import User from "../models/User.js"
 import { Purchase } from "../models/Purchase.js"
 import Stripe from "stripe"
 import Course from "../models/Course.js"
+import { CourseProgress } from "../models/CourseProgress.js"
 
 //get user data
 
@@ -87,5 +88,47 @@ export const purchaseCourse = async (req, res)=>{
 
     } catch (error) {
         res.json({ success: false, message: error.message });
+    }
+}
+
+// Update User Course Progress
+export const updateUserCourseProgress = async (req, res)=> {
+    try {
+        const userId = req.auth.userId
+        const { courseId, lectureId } = req.body
+        const progressData = await CourseProgress.findOne({ userId, courseId })
+
+        if(progressData){
+            if(progressData.lectureCompleted.includes(lectureId)){
+                return res.json({ success: true, message: 'Lecture Already Completed' })
+            }
+
+            progressData.lectureCompleted.push(lectureId)
+            await progressData.save()
+        } else {
+            await CourseProgress.create({
+                userId,
+                courseId,
+                lectureCompleted: [lectureId]
+            })
+        }
+
+        res.json({ success: true, message: 'Progress Updated' })
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
+
+
+// get User Course Progress
+export const getUserCourseProgress = async (req, res) =>{
+    try {
+        const userId = req.auth.userId
+        const { courseId } = req.body
+        const progressData = await CourseProgress.findOne({userId, courseId })
+        res.json({success: true, progressData})
+    } catch (error) {
+        res.json({ success: false, message: error.message })
     }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,8 @@
 import express from 'express'
-import { getUserData,
+import { getUserCourseProgress,
+         getUserData,
          purchaseCourse,
+         updateUserCourseProgress,
          userEnrolledCourses } from '../controllers/userController.js'
 
 const userRouter = express.Router()
@@ -8,6 +10,8 @@ const userRouter = express.Router()
 userRouter.get('/data', getUserData)
 userRouter.get('/enrolled-courses', userEnrolledCourses)
 userRouter.post('/purchase', purchaseCourse)
+userRouter.post('/update-course-progress', updateUserCourseProgress )
+userRouter.post('/get-course-progress', getUserCourseProgress )
 
 
 export default userRouter;
